a88d0df3a114cc9b28f8b559c03c32c4bd67b769
Kacper Stypik <kacperstypik@mailbox.org>
2022-06-03 10:43:43 +0200

    Change syntax in Paste model to be optional

--- pastes/models.py
+++ pastes/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class Paste(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
-    syntax = models.CharField(max_length=50)
+    syntax = models.CharField(max_length=50, blank=True)
     expiration_time = models.TimeField(null=True, blank=True)
     # folder =
     password = models.CharField(max_length=100, blank=True)
