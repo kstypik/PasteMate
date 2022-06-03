883f0660b892bf7fb811b1e41600cf369729284f
Kacper Stypik <kacperstypik@mailbox.org>
2022-06-03 13:56:32 +0200

    Add Paste Delete feature

--- pastes/urls.py
+++ pastes/urls.py
@@ -7,5 +7,6 @@ app_name = "pastes"
 urlpatterns = [
     path("<uuid:uuid>/", views.PasteDetailView.as_view(), name="detail"),
     path("<uuid:uuid>/edit/", views.PasteUpdateView.as_view(), name="update"),
+    path("<uuid:uuid>/delete/", views.PasteDeleteView.as_view(), name="delete"),
     path("", views.PasteCreateView.as_view(), name="create"),
 ]
--- pastes/views.py
+++ pastes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
 
 from .forms import PasteForm
 from .models import Paste
@@ -41,3 +41,19 @@ class PasteUpdateView(UpdateView):
 
     def get_queryset(self):
         return Paste.objects.filter(author=self.request.user)
+
+
+class PasteDeleteView(DeleteView):
+    slug_field = "uuid"
+    slug_url_kwarg = "uuid"
+    success_url = "/"
+    context_object_name = "paste"
+    template_name = "pastes/delete.html"
+
+    def get_queryset(self):
+        return Paste.objects.filter(author=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.object.title if self.object.title else "Untitled"
+        return context
