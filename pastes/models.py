f3719800c30626b94ed2878daee134d86539b480
Kacper Stypik <kacperstypik@mailbox.org>
2022-06-03 09:40:14 +0200

    Add Paste model

--- pastes/models.py
+++ pastes/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Paste(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField()
+    syntax = models.CharField(max_length=50)
+    expiration_time = models.TimeField(null=True, blank=True)
+    # folder =
+    password = models.CharField(max_length=100, blank=True)
+    burn_after_read = models.BooleanField(default=False)
+    title = models.CharField(max_length=50)
