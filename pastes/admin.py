740301622edc8b97e5c4cef6962189f4958d4adc
Kacper Stypik <kacperstypik@mailbox.org>
2022-06-04 19:03:26 +0200

    Add Pastes to Admin

--- pastes/admin.py
+++ pastes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Paste
+
+
+@admin.register(Paste)
+class PasteAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "syntax",
+        "author",
+        "burn_after_read",
+        "created",
+        "expiration_time",
+    ]
