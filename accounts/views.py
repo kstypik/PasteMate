94937016e7047292a83d9bdd2723016e9d2b0284
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-02 20:51:58 +0200

    Refactor common get_object method into mixin in accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -9,35 +9,37 @@ from .forms import AccountDeleteForm, AvatarForm, PreferencesForm, ProfileForm
 User = get_user_model()
 
 
-class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class AuthenticatedUserObjectMixin:
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ProfileUpdateView(
+    LoginRequiredMixin, SuccessMessageMixin, AuthenticatedUserObjectMixin, UpdateView
+):
     form_class = ProfileForm
     template_name = "account/profile_edit.html"
     success_message = "Your profile has been updated!"
     success_url = reverse_lazy("accounts:profile_update")
 
-    def get_object(self, queryset=None):
-        return self.request.user
 
-
-class AvatarUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class AvatarUpdateView(
+    LoginRequiredMixin, SuccessMessageMixin, AuthenticatedUserObjectMixin, UpdateView
+):
     form_class = AvatarForm
     template_name = "account/avatar_edit.html"
     success_message = "Your avatar has been set!"
     success_url = reverse_lazy("accounts:profile_update")
 
-    def get_object(self, queryset=None):
-        return self.request.user
 
-
-class AccountDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+class AccountDeleteView(
+    LoginRequiredMixin, SuccessMessageMixin, AuthenticatedUserObjectMixin, DeleteView
+):
     model = User
     form_class = AccountDeleteForm
     template_name = "account/account_delete.html"
     success_message = "Your account has been deleted."
     success_url = reverse_lazy("pastes:create")
-
-    def get_object(self, queryset=None):
-        return self.request.user
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
