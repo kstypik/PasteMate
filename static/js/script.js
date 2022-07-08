44fa3ba5c97299612155bff820641daade9e1cc1
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-08 13:30:22 +0200

    Refactor copy js function to be more reusable

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,32 @@ for (const dtElement of toRelativeDateTimes) {
     dtElement.innerHTML = relativeDatetime;
 }
 
+let getCopyHandler = (element, content, position) => {
+    let hasCopied = false;
+    handleCopy = (event) => {
+      event.preventDefault();
+      
+      
+
+      content.select();
+      content.setSelectionRange(0, 99999);
+      navigator.clipboard.writeText(content.value);
+      // rawCopy.focus();
+      
+
+      if (!hasCopied) {
+        successMsgSpan = document.createElement("span");
+        successMsgSpan.innerHTML = "Copied";
+        successMsgSpan.classList.add("text-success");
+        successMsgSpan.classList.add("mx-2");
+  
+        element.insertAdjacentElement(position, successMsgSpan);
+      }
+      hasCopied = true;
+      setTimeout(() => {
+        successMsgSpan.remove();
+        hasCopied = false;
+      }, 2000);
+    }
+    return handleCopy;
+}
