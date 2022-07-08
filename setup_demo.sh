91d09235fa86a576735d8e00f2fb2cb3c923d819
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-08 19:57:52 +0200

    Add regenerated_embed_images to demo script

--- setup_demo.sh
+++ setup_demo.sh
@@ -4,3 +4,4 @@ django-admin setup_admin
 django-admin reset_test_user
 django-admin generate_demo_pastes
 django-admin loaddata pastemate/pastes/fixtures/pastes.json
+django-admin regenerate_embed_images
