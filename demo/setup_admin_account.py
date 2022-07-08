1fed4f5426e4ff8ba479f7a10ae7746c0d06c59d
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-08 18:02:41 +0200

    Fix admin demo account from normal user to superuser

--- demo/setup_admin_account.py
+++ demo/setup_admin_account.py
@@ -2,8 +2,8 @@ from config.settings.base import env
 from pastemate.accounts.models import User
 
 if __name__ == "__main__":
-    User.objects.create(
+    User.objects.create_superuser(
         username="Admin",
-        password=env("DJANGO_DEMO_ADMIN_PASS"),
         email="admin@example.com",
+        password=env("DJANGO_DEMO_ADMIN_PASS"),
     )
