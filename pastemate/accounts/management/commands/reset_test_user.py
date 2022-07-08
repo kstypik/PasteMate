5b694e7b9181426e728442825d9499f021e4e3da
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-08 19:37:17 +0200

    Fix resetting test user command

--- pastemate/accounts/management/commands/reset_test_user.py
+++ pastemate/accounts/management/commands/reset_test_user.py
@@ -7,15 +7,15 @@ class Command(BaseCommand):
     help = "Resets test account"
 
     def handle(self, *args, **options):
-        test_account, created = User.objects.get_or_create(id=2)
-        if not created:
-            test_account.username = "test"
-            test_account.email = "test@example.com"
-            test_account.set_password("test12")
-            test_account.location = "Testland"
-            test_account.website = "https://example.com"
-            test_account.avatar = "default_avatar.png"
-
+        defaults = {
+            "username": "test",
+            "email": "test@example.com",
+            "location": "Testland",
+            "website": "https://example.com",
+            "avatar": "default_avatar.png",
+        }
+        test_account, created = User.objects.get_or_create(id=2, defaults=defaults)
+        test_account.set_password("test12")
         test_account.save()
 
         self.stdout.write("Test account reset.")
