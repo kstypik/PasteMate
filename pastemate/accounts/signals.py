2b4ffb4008b348d48e3245cf90f5468326f18917
Kacper Stypik <kacperstypik@mailbox.org>
2022-07-15 13:25:53 +0200

    Make welcome message separate signal

--- pastemate/accounts/signals.py
+++ pastemate/accounts/signals.py
@@ -10,6 +10,10 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Preferences.objects.create(user=instance)
 
+
+@receiver(post_save, sender=User)
+def send_welcome_message(sender, instance, created, **kwargs):
+    if created:
         try:
             admin_account = User.objects.get(id=1)
         except User.DoesNotExist:
@@ -17,11 +21,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 
         subject = "Welcome to PasteMate!"
         content = f"""Hello {instance.username},
-Good to see that you have decided to join our community!
-If you have any questions regarding the website, please feel free to contact us.
+    Good to see that you have decided to join our community!
+    If you have any questions regarding the website, please feel free to contact us.
 
-Kind regards,
-PasteMate team"""
+    Kind regards,
+    PasteMate team"""
         Message.new_message(
             from_user=admin_account,
             to_users=[instance],
