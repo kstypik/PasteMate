5ec843a40115ea241d5e476184fb7415a8f2b8b1
Kacper Stypik <kacperstypik@mailbox.org>
2022-08-05 19:58:14 +0200

    Change PasteSerializer to standard model serializer

--- pastemate/pastes/serializers.py
+++ pastemate/pastes/serializers.py
@@ -3,13 +3,12 @@ from rest_framework import serializers
 from pastemate.pastes.models import Paste
 
 
-class PasteSerializer(serializers.HyperlinkedModelSerializer):
+class PasteSerializer(serializers.ModelSerializer):
     paste_url = serializers.CharField(source="get_absolute_url", read_only=True)
 
     class Meta:
         model = Paste
         fields = [
-            "url",
             "uuid",
             "title",
             "created",
@@ -17,15 +16,12 @@ class PasteSerializer(serializers.HyperlinkedModelSerializer):
             "expiration_date",
             "exposure",
             "syntax",
-            "hits",
             "paste_url",
         ]
         read_only_fields = [
-            "url",
             "uuid",
             "created",
             "filesize",
             "expiration_date",
-            "hits",
             "paste_url",
         ]
