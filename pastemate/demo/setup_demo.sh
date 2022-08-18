151f927dfb772c2e85863c2daffe16c910d95f70
Kacper Stypik <kacperstypik@mailbox.org>
2022-08-18 11:36:03 +0200

    Switch from manage.py to django-admin in demo: :

--- pastemate/demo/setup_demo.sh
+++ pastemate/demo/setup_demo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -r media/embed
-python manage.py reset_db
-python manage.py setup_admin
-python manage.py reset_test_user
-python manage.py generate_demo_pastes
-python manage.py loaddata pastemate/pastes/fixtures/pastes.json
-python manage.py regenerate_embed_images
+django-admin reset_db
+django-admin setup_admin
+django-admin reset_test_user
+django-admin generate_demo_pastes
+django-admin loaddata pastemate/pastes/fixtures/pastes.json
+django-admin regenerate_embed_images
 cp pastemate/demo/demo_base_template.html templates/_base.html
